ca2a8a8b0296803c89556363f95c6352c889785e
rflw <rflw@protonmail.com>
2024-08-18 19:47:14 +0200

    update question component

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,20 +1,30 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
 import { useTriviaStore } from '@/store/useTrivia';
-import { computed, ComputedRef, toRefs } from 'vue';
+import { computed, ComputedRef } from 'vue';
+import { QuestionEntity } from '@/types/trivia';
 
 const triviaStore = useTriviaStore();
 const { currentQuestion } = storeToRefs(triviaStore);
-const { question, correct_answer, incorrect_answers } = toRefs(currentQuestion.value);
 
-// TODO: randomize
-const combinedAnswers: ComputedRef<string[]> = computed(() => ([correct_answer.value, ...incorrect_answers.value]))
+function shuffleAnswers({ correct_answer, incorrect_answers }: QuestionEntity ): string[] {
+  const totalAnswersCount = incorrect_answers.length + 1; // +1 adds correct answer to incorrect answers
+  const randomIndex = Math.floor(Math.random() * totalAnswersCount);
+
+  return [
+    ...incorrect_answers.slice(0, randomIndex),
+    correct_answer,
+    ...incorrect_answers.slice(randomIndex)
+  ];
+}
+
+const allAnswers: ComputedRef<string[]> = computed(() => shuffleAnswers(currentQuestion.value));
 </script>
 
 <template>
-  <template v-if="currentQuestion">
-    <p>{{ question }}</p>
-    <p>{{ currentQuestion.difficulty }}</p>
-    <button v-for="(answer, index) in combinedAnswers" :key="index" v-text="answer" />
-  </template>
+  <div v-if="currentQuestion" class="question-container">
+    <p class="question">{{ currentQuestion.question }}</p>
+    <p class="">{{ currentQuestion.difficulty }}</p>
+    <button v-for="(answer, index) in allAnswers" :key="index" v-text="answer" class="button" />
+  </div>
 </template>

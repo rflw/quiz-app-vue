744ac6896b186dcb373c38ad489aeb6db08d7d34
rflw <rflw@protonmail.com>
2024-08-19 02:26:50 +0200

    add chart

---
+++ src/types/chart.ts
@@ -0,0 +1,4 @@
+import type { ChartData } from 'chart.js';
+
+type ChartType = 'bar';
+export type BarChartData = ChartData<ChartType>;
--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,3 +1,32 @@
+<script setup lang="ts">
+import { useAppStore } from '@/store/useApp';
+import { storeToRefs } from 'pinia';
+import BarChart from '@/components/BarChart.vue';
+import type { BarChartData } from '@/types/chart';
+import { ref, Ref } from 'vue';
+
+const appStore = useAppStore();
+const { totalScoreRatioPercent, totalScoreRatio } = storeToRefs(appStore);
+const options = { responsive: true };
+
+// TODO: update by real data
+const chartData: Ref<BarChartData> = ref({
+  labels: [ 'January', 'February', 'March' ],
+  datasets: [
+    { label: 'tes', data: [40, 20, 12] },
+    { label: 'foo', data: [20, 50, 100] }
+    ]
+});
+</script>
+
 <template>
   Results view
+  <br>
+  totalScoreRatio: {{ totalScoreRatioPercent }}%
+  <br>
+  correct answers: {{ totalScoreRatio.correctAnswers }}
+  <br>
+  all answers: {{ totalScoreRatio.allAnswers }}
+
+  <BarChart :options="options" :data="chartData"/>
 </template>
